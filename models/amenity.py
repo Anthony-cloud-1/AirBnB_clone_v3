3d4895a935f6209d3b0df4eb38dc9a5564c068b7
Anthony Sackey <tonysack13@gmail.com>
2024-04-30 22:31:11 +0000

    API

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ holds class Amenity"""
 import models
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
 
 class Amenity(BaseModel, Base):
     """Representation of Amenity """
