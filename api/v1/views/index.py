e6bac62462f92f30e62c964a1621a7f0f43966f3
calvin <calvinsewornu123@gmail.com>
2024-05-02 03:41:42 +0000

    api air 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Contains the index view for the API.'''
+'''Contains index view for the API.'''
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
@@ -13,7 +13,7 @@ def status():
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stat():
-    """returns the number of each objects by type"""
+    """returns number of each objects by type"""
     return jsonify(
         amenities=storage.count('Amenity'),
         cities=storage.count('City'),
