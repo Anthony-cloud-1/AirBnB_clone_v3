92be31bbb5940ca69bbb3fa794e9c76d1bd86942
calvin <calvinsewornu123@gmail.com>
2024-05-02 03:44:11 +0000

    api air 3

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -74,4 +74,4 @@ def put_city(city_id):
             setattr(obj, k, v)
 
     storage.save()
-    return make_response(jsonify(obj.to_dict()), 200)
+    return _response(jsonify(obj.to_dict()), 200)
